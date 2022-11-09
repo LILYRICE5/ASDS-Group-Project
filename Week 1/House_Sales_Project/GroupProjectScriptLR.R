1a8bc89edda9c434a07fe732cb32fb0e708ad2f3
LILYRICE5 <lrice@tcd.ie>
2022-11-09 11:09:49 +0000

    cleaning up Week 1 workings

--- Week 1/House_Sales_Project/GroupProjectScriptLR.R
+++ Week 1/House_Sales_Project/GroupProjectScriptLR.R
@@ -1,13 +1,20 @@
 ##### Workings - Lily
 
+# First, loading data into my R Studio session - doing "read.table" as this 
+# pulls the data out already separated into columns. If I was to do "read.csv"
+# it would pull the data out without separating into columns, as there are no
+# commas between the data for it to recognise as the separation between columns
+
 data <- read.table("https://raw.githubusercontent.com/gedeck/practical-statistics-for-data-scientists/master/data/house_sales.csv")
+
+# Then explore the data - below are some exploratory functions I use to do this:
+
 summary(data)
 head(data)
 list(data)
 
 install.packages("dplyr")
 
-separate(data, col)
 
 
 
